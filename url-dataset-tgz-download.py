dbd817da0c1886d9d486557d487e1240c139a9e7
Lakshika Parihar <31981299+lakshikaparihar@users.noreply.github.com>
2021-03-19 12:47:56 +0530

    Update url-dataset-tgz-download.py

--- url-dataset-tgz-download.py
+++ url-dataset-tgz-download.py
@@ -8,7 +8,7 @@ URL = ""
 def fetch_data(url = URL,path=PATH):
     os.makedirs(path,exist_ok=True)
     tgz_path = os.path.join(path,"dataset_name.tgz")
-    urllib.request.urlretrieve(path,tgz_path)
+    urllib.request.urlretrieve(url,tgz_path)
     tgz_file = tarfile.open(tgz_path)
     tgz_file.extractall(path=path)
     tgz_file.close()
