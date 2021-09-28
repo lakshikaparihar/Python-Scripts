0f4f2d78e8368df280c98ac3bd8b3dcddb486905
Lakshika Parihar <31981299+lakshikaparihar@users.noreply.github.com>
2021-09-28 11:11:24 +0530

    Update chuck_noris_unary_message.py

--- chuck_noris_unary_message.py
+++ chuck_noris_unary_message.py
@@ -1,4 +1,7 @@
-```Write a program that takes an incoming message as input and displays as output the message encoded using Chuck Norris’ method.
+```
+Binary with 0 and 1 is good, but binary with only 0, or almost, is even better! Originally, this is a concept designed by Chuck Norris to send so called unary messages.
+
+Write a program that takes an incoming message as input and displays as output the message encoded using Chuck Norris’ method.
 
 Here is the encoding principle:
 
