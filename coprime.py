891d9cf6e104119eaa70d51937886fe92ec7b5d0
Lakshika Parihar <31981299+lakshikaparihar@users.noreply.github.com>
2021-10-01 09:57:35 +0530

    Create coprime.py

---
+++ coprime.py
@@ -0,0 +1,13 @@
+```
+We are given two integers a and b. For those two integers we look for positive integers which divide both a and b (without leaving a remainder). 
+If there does not exist such an integer besides 1, we call a and b coprime.
+```
+
+import sys
+import math
+
+a = int(input())
+b = int(input())
+if math.gcd(a,b) != 1:
+    print("false")
+else: print("true")
